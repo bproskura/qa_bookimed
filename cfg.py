4931be01a242100926d029bbb746b44e580c57c2
bproskura <luchapersona@gmail.com>
2024-06-22 13:49:11 +0300

    SetUp project

--- cfg.py
+++ cfg.py
@@ -7,33 +7,7 @@ load_dotenv(override=False)
 # ### SECURITY TOKENS ###
 TG_BOT_TOKEN: str = os.getenv("TG_BOT_TOKEN")
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")
-KING_URL = "https://slotoking.ua"
-SEVENS_URL = "https://777.ua"
-FUNRIZE_URL = "https://funrize.com"
-KING_LANDINGS_URL = {
-    "kg-best": "https://kg-best-spin.lets-fun.top",
-    "kg-bonus": "https://kg-bonus.fun-spin.top",
-    "kg-fortune": "https://kg-fortune-boxes.fun-spin.top",
-    "kg-lucky-bonus": "https://kg-lucky-bonus.lets-fun.top",
-    "kg-lucky-wheel": "https://kg-lucky-wheel.fun-spin.top",
-    "kg-welcome": "https://kg-welcome.lets-fun.top",
-    "kg-prize": "https://kg-prize.lets-fun.top",
-    "kg-slot": "https://kg-slot.lets-fun.top/",
-}
-
-SEVENS_LANDINGS_URL = {
-    "lady-wheel": "https://777-lady-wheel.lets-fun.top",
-    "adventure-wheel": "https://777-adventure-wheel.lets-fun.top",
-    "welcome-lets": "https://777-welcome.lets-up.fun",
-    "welcome-bonus": "https://777_welcome_bonus_pack.lets-fun.top",
-}
-
-FUNRIZE_LANDINGS_URL = {
-    "funrize_lp_one": "https://promo.funrize.com/lp1",
-    "funrize_lp_two": "https://promo.funrize.com/lp2",
-    "funrize_lp_three": "https://promo.funrize.com/lp3",
-    "funrize_lp_four": "https://promo.funrize.com/lp4",
-}
+BASE_URL: str = "https://us-uk.bookimed.com"
 
 LANDINGS_SET_COOKIE = "__gmbrV2"
 WAIT_PAGE_LOAD_TIMEOUT = 10000
