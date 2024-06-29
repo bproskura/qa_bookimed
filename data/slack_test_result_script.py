951045594eb0c9b85b36ad8bb2c4dab3e7646a44
bproskura <luchapersona@gmail.com>
2024-06-29 13:08:51 +0300

    Add slack script

--- data/slack_test_result_script.py
+++ data/slack_test_result_script.py
@@ -1,4 +1,7 @@
+import json
 import xml.etree.ElementTree as ET
+
+import requests
 
 
 def extract_test_results(file_path):
@@ -27,12 +30,35 @@ def extract_test_results(file_path):
         return 0, 0, []
 
 
+def send_slack_message(webhook_url, total_tests, total_failures, failed_tests):
+    headers = {'Content-Type': 'application/json'}
+
+    # Prepare the message
+    message = "✅ Tests completed:\n"
+    message += "\n"
+    message += f"TOTAL_TESTS={total_tests}\n"
+    message += f"TOTAL_FAILURES={total_failures}\n"
+
+    # Append failed tests
+    message += "FAILED_TESTS:\n\n"
+    for test in failed_tests:
+        message += f"{test}\n\n"
+
+    payload = {'text': message.strip()}  # Strip to remove any leading/trailing whitespace
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
+
+    if response.status_code != 200:
+        print(f"Failed to send Slack message: {response.status_code}, {response.text}")
+    else:
+        print("Slack message sent successfully!")
+
+
 if __name__ == "__main__":
-    file_path = 'results/test-results.xml'
+    file_path = 'results/test-results.xml'  # Path to your XML test results file
     total_tests, total_failures, failed_tests = extract_test_results(file_path)
 
-    print(f"TOTAL_TESTS={total_tests}")
-    print(f"TOTAL_FAILURES={total_failures}")
-    print("FAILED_TESTS:")
-    for failed_test in failed_tests:
-        print(failed_test)
+    # Slack webhook URL
+    webhook_url = "https://hooks.slack.com/services/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"
+
+    # Send Slack message with test results
+    send_slack_message(webhook_url, total_tests, total_failures, failed_tests)
